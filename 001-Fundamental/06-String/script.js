9c5a72c0b077255809779c58a903de4e2b1bdc34
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2024-05-23 19:14:40 +0700

    add(string): string to js foundation

---
+++ 001-Fundamental/06-String/script.js
@@ -0,0 +1,9 @@
+let firstName = "Lwin";
+let lastName = "Moe Paing";
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let x = "10";
+let y = "20";
+let sum = `${x} + ${y} = 30`;
+console.log(sum);
